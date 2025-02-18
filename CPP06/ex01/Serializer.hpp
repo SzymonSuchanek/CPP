b0557a98d41f15a3e92dd874137ca194f5bba91b
SzymonSuchanek <jus20be20+1@gmail.com>
2025-02-18 23:21:40 +0100

    update

--- CPP06/ex01/Serializer.hpp
+++ CPP06/ex01/Serializer.hpp
@@ -1,9 +1,13 @@
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
+# include <string>
 # include <stdint.h>
 
-# include "Data.hpp"
+struct Data {
+    std::string type;
+    int value;
+};
 
 class Serializer {
 
