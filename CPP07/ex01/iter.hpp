b03eefd7b860147223f5f5fceb9648ca2d28be07
SzymonSuchanek <jus20be20+1@gmail.com>
2025-02-19 03:36:24 +0100

    update

---
+++ CPP07/ex01/iter.hpp
@@ -0,0 +1,12 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <cstddef>
+
+template <typename T, typename Func>
+void iter(T& array, size_t len, Func func) {
+    for (size_t i = 0; i < len; i++)
+        func(array[i]);
+}
+
+#endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,28 +6,36 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 19:11:55 by ssuchane          #+#    #+#             */
-/*   Updated: 2025/02/19 02:52:04 by marvin           ###   ########.fr       */
+/*   Updated: 2025/02/19 03:21:59 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "whatever.hpp" 
+#include "iter.hpp"
 
-int main(void) {
-    int a = 2;
-    int b = 3;
-    
-    ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    
-    return 0;
+void printInt(const int& n) {
+    std::cout << n << " ";
+}
+
+void toUpper(char &c) {
+    if (c >= 'a' && c <= 'z') {
+        c -= 32;
     }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    size_t len = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    iter(arr, len, printInt);
+    std::cout << std::endl;
+
+    char str[] = "hello";
+    size_t strLen = sizeof(str) - 1;
+
+    iter(str, strLen, toUpper);
+    std::cout << "Modified string: " << str << std::endl;
+
+    return 0;
+}
